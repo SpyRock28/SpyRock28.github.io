5d8fc28fa51544b65e4ef20a96fb780f2391e77c
SpyRock28 <imbradensteiner@gmail.com>
2018-09-20 16:45:47 -0600

    Added email from javascript. not quite working with HTML.

--- app.js
+++ app.js
@@ -122,6 +122,23 @@ function refreshEmployees() {
   });
 }
 
+// let emailBody = '<#DOCTYPE html><html><head></head>' +
+//                 '<body><header><div style="color: blue; height: 150; width: 80%"></div></header>' +
+//                 '<table style="width=80%"><tr><th>Firstname</th><th>Lastname</th><th>Age</th></tr>' +
+//                 "<tr><td>Jill</td><td>Smith</td><td>50</td></tr>" +
+//                 "<tr><td>Eve</td><td>Jackson</td><td>94</td></tr></table>" +
+//                 "<footer><ul><li>1</li><li>2</li>" +
+//                 "<li>3</li><li>4</li></ul></footer></body></html>";
+// let emailAddress = "todd.poulson@us.af.mil";
+// let subject = "Test Email Proof of Concept";
+// let openEmail = document.getElementById("openEmail");
+//
+// openEmail.onsubmit = () => {
+//   window.open('mailto:' + emailAddress + '?subject=' + subject + '&body=' + body);
+//
+//   return false;
+// };
+
 //Searching with PDF.js
 
 var searchText = "JavaScript";
